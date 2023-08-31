dda8a1b786146dc7a01614c702a93fd5990aa9e7
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-08-31 11:10:22 -0300

    navbar javascript final
    se termino la logica del navbar

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-window.addEventListener("scroll", function(){
-    let top = document.querySelector(".top");
-    let principal = document.querySelector(".principal");
-    top.classList.toggle("borrar",window.scrollY>0);
-    principal.classList.toggle("cambiar",window.scrollY>0);
+let top1 = document.querySelector('.top1');
+let principal = document.querySelector('.principal');
+
+
+window.addEventListener('scroll', function(){
+    // top.classList.toggle("borrar",window.scrollY>0);
+    // principal.classList.toggle("cambiar",window.scrollY>0);
+    if (window.scrollY>250){
+        console.log(window.scrollY);
+        
+        top1.style.display = 'none';
+        principal.style.top = '0';
+        principal.style.backgroundColor = "rgba(69, 64, 123,1)" ;
+        
+    }else{
+        top1.style.display = 'flex';
+        principal.style.top = '40px';
+        principal.style.backgroundColor = "rgba(69, 64, 123,.9)"
+    }
 
 });
