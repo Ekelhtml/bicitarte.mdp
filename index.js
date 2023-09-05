f78cd86ffd6fd3b001911666d5d3c5d6a3f9475d
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-09-05 15:19:14 -0300

    modificacion variables
    correccion de variables para conectar con la rama principal

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+
+// Navbar
 let top1 = document.querySelector('.top1');
 let principal = document.querySelector('.principal');
 
-
 window.addEventListener('scroll', function(){
-    // top.classList.toggle("borrar",window.scrollY>0);
-    // principal.classList.toggle("cambiar",window.scrollY>0);
+
     if (window.scrollY>250){
         console.log(window.scrollY);
         
@@ -17,5 +17,4 @@ window.addEventListener('scroll', function(){
         principal.style.top = '40px';
         principal.style.backgroundColor = "rgba(69, 64, 123,.9)"
     }
-
 });
